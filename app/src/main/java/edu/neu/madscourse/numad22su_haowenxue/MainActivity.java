90340d6954f7474eb7a97882514d9488ce345a3c
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-05-15 10:39:01 -0700

    re-init empty activity

---
+++ app/src/main/java/edu/neu/madscourse/numad22su_haowenxue/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madscourse.numad22su_haowenxue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
