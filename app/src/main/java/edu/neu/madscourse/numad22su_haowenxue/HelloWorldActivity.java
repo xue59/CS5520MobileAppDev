eda3043f5afb9d35707d5b6c66c5ffcd85859000
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-05-28 20:31:33 -0700

    finish assignment2 add a back button

---
+++ app/src/main/java/edu/neu/madscourse/numad22su_haowenxue/HelloWorldActivity.java
@@ -0,0 +1,29 @@
+package edu.neu.madscourse.numad22su_haowenxue;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+
+        ActionBar actionBar = getSupportActionBar(); // calling the bar
+        actionBar.setDisplayHomeAsUpEnabled(true); // showing the bar
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
