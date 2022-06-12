8d6f13c35783d935e6206182ab6bce1bed4c0c32
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-06-12 02:44:20 -0700

    finish basic requeiment, need layout adjuested

--- app/src/main/java/edu/neu/madscourse/numad22su_haowenxue/MainActivity.java
+++ app/src/main/java/edu/neu/madscourse/numad22su_haowenxue/MainActivity.java
@@ -12,7 +12,7 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     private static final String MyTAG="LogTag";
-    private Button btnAboutMe, btnHelloWorld, btnClicky, btnLinkCollector;
+    private Button btnAboutMe, btnHelloWorld, btnClicky, btnLinkCollector, btnPrimeTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         btnHelloWorld = (Button) findViewById(R.id.btnHelloWorld); //select the helloWorld button by ID
         btnClicky = (Button) findViewById(R.id.btnClicky); //select clicky button
         btnLinkCollector = (Button) findViewById(R.id.btnLinkCollector);
+        btnPrimeTime = (Button) findViewById(R.id.btnPrimeTime);
 
         btnAboutMe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {openLinkCollectorActivity();}
         });
 
+        btnPrimeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openPrimeTimeActivity();}
+        });
+
     }
     public void openHelloWorldActivity() {
         Intent intent = new Intent(this, HelloWorldActivity.class);
@@ -64,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openLinkCollectorActivity(){
         Intent intent = new Intent(this, LinkCollectorActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPrimeTimeActivity(){
+        Intent intent = new Intent(this, PrimeTimeActivity.class);
         startActivity(intent);
     }
 }
