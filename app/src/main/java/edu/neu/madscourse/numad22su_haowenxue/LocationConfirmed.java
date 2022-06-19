18f3db8ef45140058824c9aea979ec3772ed2796
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-06-19 11:15:25 -0700

    part fiish 11:15

--- app/src/main/java/edu/neu/madscourse/numad22su_haowenxue/LocationConfirmed.java
+++ app/src/main/java/edu/neu/madscourse/numad22su_haowenxue/LocationConfirmed.java
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.LocationRequest;
 import android.util.Log;
 
 import android.Manifest;
@@ -19,6 +20,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,11 +34,15 @@ public class LocationConfirmed extends AppCompatActivity implements LocationList
     Button btnResetDist;
     TextView textLatitude, textLongtitude, textTotalDistance;
     LocationManager locationManager;
+    Switch switchApproximate;
+    Boolean switchBolean = false;
     ArrayList<ArrayList<Double>> locHistory = new ArrayList<ArrayList<Double>>(); //store location history {{Longitude, Latitude}, {}....}
     Double totalDistance = 0.0;
     DecimalFormat df = new DecimalFormat("0.000");
+    DecimalFormat df8 = new DecimalFormat("0.00000000");
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,10 +55,27 @@ public class LocationConfirmed extends AppCompatActivity implements LocationList
         textLongtitude = findViewById(R.id.textLongitude);
         textTotalDistance = findViewById(R.id.textTotalDistance);
         btnResetDist   = findViewById(R.id.btnResetDist);
+        switchApproximate = findViewById(R.id.btnApproximate);
         btnResetDist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resetDistance();
+            }
+        });
+        switchApproximate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Log.d("switch:", "Enable approximate");
+                    switchBolean = true;
+                    totalDistance = 0.0;
+                    textTotalDistance.setText("Please disable approximate \nfor accurate distance display!");
+                    getLocation();
+                }else{
+                    switchBolean = false;
+                    Log.d("switch:", "Enable precise location");
+                    getLocation();
+                }
             }
         });
 
@@ -59,13 +83,9 @@ public class LocationConfirmed extends AppCompatActivity implements LocationList
         if(ContextCompat.checkSelfPermission(LocationConfirmed.this, Manifest.permission.ACCESS_FINE_LOCATION)
           != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(LocationConfirmed.this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION}, 100);
-            getLocation();
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
         }
         getLocation();
-
-
-
 
     }
     //if the reset button is clicked distance is reset & location is saved with current locaiton only.
@@ -78,7 +98,12 @@ public class LocationConfirmed extends AppCompatActivity implements LocationList
         curLatLong.add(curLongtitude);
         locHistory.add(curLatLong);
         totalDistance = 0.0;
-        textTotalDistance.setText("0.0 km");
+        if(switchBolean == true) {
+            // turn on approximate
+            textTotalDistance.setText("Please disable approximate \nfor accurate distance display!");
+        } else{
+            textTotalDistance.setText("0.0 km");
+        }
         printLocationHistory(locHistory);
         if(locHistory.size() ==1){
             Log.d("Clicked: ", "locHistory reseted & total distance cleared!");
@@ -89,12 +114,27 @@ public class LocationConfirmed extends AppCompatActivity implements LocationList
     //function for update the location manager(latitude, longtitude)
     @SuppressLint({"MissingPermission", "ServiceCast"})
     private void getLocation(){
+        if (ContextCompat.checkSelfPermission(LocationConfirmed.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            this.finish();
+            Toast.makeText(this, "Precision location Denied. Please Grant precision location in Setting.", Toast.LENGTH_SHORT).show();
+        }
         try {
             locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
 //            Criteria criteria = new Criteria();
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,500,3,LocationConfirmed.this);
+            Log.d("Try provider list ", ""+ locationManager.getAllProviders());
         } catch (Exception e){
             Log.d("LocationConfirmed ERROR: ", "getLocation Exception ERROR!!!");
+            this.finish();
+            Toast.makeText(this, "Precision location Denied. Please Grant precision location.", Toast.LENGTH_SHORT).show();
+//            Log.d("Exception All provider list ", ""+ locationManager.getAllProviders());
+//            LocationRequest locationRequest = new LocationRequest();
+//            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+//            locationRequest.setInterval(500);
+//            Log.d("LocationConfirmed ERROR: ", ""+LocationManager.getAllProviders());
+//            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,500,3,LocationConfirmed.this);
+
         }
     }
 
@@ -113,21 +153,27 @@ public class LocationConfirmed extends AppCompatActivity implements LocationList
     public void onLocationChanged(@NonNull Location location) {
 //        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
         Log.d("Location String:", ""+location.getLatitude()+","+location.getLongitude());
-        textLatitude.setText(""+location.getLatitude());
-        textLongtitude.setText(""+location.getLongitude());
-        ArrayList<Double> curLocation = new ArrayList<Double>();
-        curLocation.add((Double) location.getLatitude());
-        curLocation.add((Double) location.getLongitude());
-        Log.d("Location Double:", ""+curLocation.get(0) + ","+ curLocation.get(1));
-        locHistory.add(curLocation);
-        printLocationHistory(locHistory);
-        if (locHistory.size() >= 2) {
-            totalDistance = calculateDistance(totalDistance, locHistory.get(locHistory.size() - 1), locHistory.get(locHistory.size() - 2)); //get the last two newly added location points
-        } else {
-            totalDistance = 0.0;
-        }
+        if(switchBolean == true){
+            // turn on approximate location
+            textLatitude.setText(""+df8.format(location.getLatitude()*0.999998));
+            textLongtitude.setText(""+df8.format(location.getLongitude()*0.999997));
+        }else{
+            // turn on precise location
+            textLatitude.setText(""+df8.format(location.getLatitude()));
+            textLongtitude.setText(""+df8.format(location.getLongitude()));
+            ArrayList<Double> curLocation = new ArrayList<Double>();
+            curLocation.add((Double) location.getLatitude());
+            curLocation.add((Double) location.getLongitude());
+            Log.d("Location Double:", ""+curLocation.get(0) + ","+ curLocation.get(1));
+            locHistory.add(curLocation);
+            printLocationHistory(locHistory);
+            if (locHistory.size() >= 2) {
+                totalDistance = calculateDistance(totalDistance, locHistory.get(locHistory.size() - 1), locHistory.get(locHistory.size() - 2)); //get the last two newly added location points
+            } else {
+                totalDistance = 0.0;
+            }
             textTotalDistance.setText(""+df.format(totalDistance)+" km");
-
+        }
 
     }
 
